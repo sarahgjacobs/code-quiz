46e350f54f029d39a80fac50d8fd23c2cb1687e1
Sarah Jacobs <sgjacobs14@gmail.com>
2022-11-16 10:31:28 -0500

    fixed the timer and some css

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,19 @@
 
 var codersIntro = document.querySelector("#start-section")
 var questionsEl = document.getElementById("quiz-section")
+var endEl = document.getElementById("end-section")
+let finalName = document.querySelector("#username");
 var questionCount;
+let scoreList = [];
+const highscoresEl = document.querySelector("#high-scores");
+let scoreListEl = document.querySelector(".score-list");
 
-const questions = [ 
+
+const questions = [
     {
         question: "What is Javascript primarily used for?",
-        answers: ["1. <outlining the website>", "2. <adding design>", "3. <adding user interface>", "4. <all of the above>"],
-        correctAnswer: "3. <adding user interface>"
+        answers: ["1. Outlining the website", "2. Adding design", "3. Adding user interface", "4. All of the above"],
+        correctAnswer: "3. Adding user interface"
     },
     {
         question: "How do you write 'Hello World' in an alert box?",
@@ -20,9 +26,9 @@ const questions = [
         correctAnswer: "3. if (i == 5)"
     },
     {
-        question: "How does a FOR loop start?",
-        answers: ["1. for (i <= 5; i++)", "2. for (i = 0; i <= 5)", "3. for i = 1 to 5", "4. for (i = 0; i <= 5; i++)"],
-        correctAnswer: "4. for (i = 0; i <= 5; i++)"
+        question: "Which class hides something?",
+        answers: ["1. hidden", "2. gohide", "3. hidenow", "4. hide"],
+        correctAnswer: "4. hide"
     },
     {
         question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
@@ -32,19 +38,20 @@ const questions = [
 ];
 
 document.getElementById("start").addEventListener("click", startQuiz)
-// const beginning = document.querySelector("#quizBox");
 
 
 var score = 0;
 
 
 function startQuiz() {
-    codersIntro.setAttribute("class", "hide") 
+    codersIntro.setAttribute("class", "hide")
     questionsEl.removeAttribute("class")
     questionCount = 0;
 
     countDown();
     setQuestion(questionCount);
+
+    
 }
 
 // SET QUESTION FUNCTION
@@ -52,41 +59,49 @@ function setQuestion(id) {
     if (id < questions.length) {
         document.querySelector(".question").textContent = questions[id].question;
         document.querySelector(".answer-box").innerHTML = ""
-        questions[id].answers.forEach(function(answer){
+        questions[id].answers.forEach(function (answer) {
             var button = document.createElement("button")
             button.textContent = answer;
             button.setAttribute("value", answer)
-            button.onclick = function() {
-            if (this.value !== questions[id].correctAnswer) {
-                console.log('wrong!')
-                totalTime -=10;
-            } else {
-                score++;
-            }
-            questionCount++; setQuestion(questionCount)
+            button.onclick = function () {
+                if (this.value !== questions[id].correctAnswer) {
+                    console.log('wrong!')
+                    totalTime -= 10;
+                } else {
+                    score++;
+                }
+                questionCount++; setQuestion(questionCount);
             }
             document.querySelector(".answer-box").appendChild(button)
         })
-        
+
     } else {
         console.log("end game")
         questionsEl.setAttribute("class", "hide")
-    }
+        endEl.removeAttribute("class")
+        highscoresEl.removeAttribute("class")
 }
+    }
+
+
+
+
+
+
 
 var totalTime = 60;
 function countDown() {
     var timeInterval = setInterval(function () {
         document.getElementById("timer").textContent = totalTime
         totalTime--;
-        
 
-    if(totalTime === 0) {
-        clearInterval(timeInterval)
-        // endMessage();
-    }
 
-}, 1000);
+        if (totalTime === 0) {
+            clearInterval(timeInterval)
+            // endMessage();
+        }
+
+    }, 1000);
 }
 
 
@@ -102,14 +117,17 @@ function countDown() {
 // };
 
 
-// function saveScore() {
-//     // Save related form data as an object
-//     var highScore = {
-//         user: userName,
-//         grade: grade.value,
-//         comment: comment.value.trim()
-//     };
-//     // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-//     localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-// }
+function saveScore() {
+    // Save related form data as an object
+    highscoresEl.style.display = "block";
+    var highScore = {
+        user: userName,
+        comment: comment.value.trim()
+    };
+    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+    localStorage.setItem("username", JSON.stringify(username));
+    let storedScoreList = JSON.parse(localStorage.getItem("score-List"));
+
+    console.log(username)
+}
 
